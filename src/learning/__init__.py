a91e7a51965a090128438c4d51ccb5103f6d8ab3
Felix Koppe <fkoppe@web.de>
2025-11-08 15:53:12 +0100

    Add meta learning management system

---
+++ src/learning/__init__.py
@@ -0,0 +1,5 @@
+"""Learning subsystem entry points."""
+
+from .manager import LearningEvent, LearningManager, get_learning_manager
+
+__all__ = ["LearningEvent", "LearningManager", "get_learning_manager"]
