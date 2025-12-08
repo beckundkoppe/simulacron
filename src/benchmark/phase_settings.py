462c45fc15e044fd80a5ef9cde8191fcf41103e5
Felix Koppe <fkoppe@web.de>
2025-12-08 14:30:42 +0100

    Work

--- src/benchmark/phase_settings.py
+++ src/benchmark/phase_settings.py
@@ -320,7 +320,7 @@ RUNNER_CONFIGS: Dict[str, RunnerConfig] = {
             ModelTeams.Remote.GPT5_NANO,
             ModelTeams.Remote.GPT5_MINI,
         ],
-        allowed_phases=["openai", "backend", "baseline-solo", "baseline-mix", "feature"],
+        allowed_phases=["openai"], #"backend", "baseline-solo", "baseline-mix", "feature"
     ),
     "zedim-pc": RunnerConfig(
         allowed_model_teams=[
